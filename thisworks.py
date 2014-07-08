9678d9cbfb3a4317ef4c111753941b765a738eae
MinRK <benjaminrk@gmail.com>
2014-07-08 13:33:55 -0500

    redirect current browser to the subprocess server
    - make %%thisworks code-cell a no-op in the subprocess
    via ENV[‘THISWORKS’]
    
    - buffer subprocess output to sys.stdout

--- thisworks.py
+++ thisworks.py
@@ -3,9 +3,13 @@ An IPython extension for specifying requirements
 """
 import os
 import sys
-from subprocess import check_call
+import socket
+import time
+from subprocess import Popen, PIPE, STDOUT, CalledProcessError
 from itertools import dropwhile
 from IPython.core.magic import magics_class, cell_magic, Magics
+
+from IPython.display import display, Javascript
 
 HASHSTACK_TEMPLATE=\
 """
@@ -15,6 +19,37 @@ extends:
    key: 'git:{git_id}'
    file: {platform}.yaml
 """
+
+REDIRECT_TEMPLATE = """
+// disable 'unsaved changes' dialog
+window.onbeforeunload = function () {{}};
+window.location = "{url}";
+"""
+
+
+def buffer_output(process):
+    """Buffer the output of a Popen object to sys.stdout."""
+    # show stdout as it comes
+    while process.poll() is None:
+        line = process.stdout.readline().decode('utf8', 'replace')
+        sys.stdout.write(line)
+        sys.stdout.flush()
+
+    # finish off everything else
+    remaining = process.stdout.read().decode('utf8', 'replace')
+    if remaining:
+        sys.stdout.write(remaining)
+        sys.stdout.flush()
+
+
+def call_buffered(cmd, **kwargs):
+    """Call a subprocess and buffer its output to sys.stdout."""
+
+    process = Popen(cmd, stdout=PIPE, stderr=STDOUT, **kwargs)
+    buffer_output(process)
+
+    if process.returncode != 0:
+        raise CalledProcessError("%s failed with status %i" % (cmd[0], process.returncode))
 
 
 def assemble_hashstack(command, profile_contents):
@@ -45,6 +80,27 @@ def transform_using(profile_contents):
         profile_contents = assemble_hashstack(command, list(non_empty_lines))
     return profile_contents
 
+def random_port():
+    """A random available port."""
+    sock = socket.socket()
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return port
+
+def wait_for_port(ip, port, timeout=2):
+    """wait for a peer to show up at a port"""
+    tic = time.time()
+    while time.time() - tic < timeout:
+        try:
+            socket.create_connection((ip, port))
+        except socket.error:
+            time.sleep(0.1)
+        else:
+            return
+    raise Exception("Notebook never seemed to start.")
+
+
 @magics_class
 class ThisWorks(Magics):
 
@@ -56,9 +112,15 @@ class ThisWorks(Magics):
         
         Usage:
         
-            %thisworks profile_name
+            %%thisworks notebook_name
+
+            hashdist: stuff
 
         """
+        if os.environ.get('THISWORKS', False):
+            print("You are currently in a thisworks profile")
+            return
+
         notebook_name = line.strip()
         if notebook_name:
             profile_name = notebook_name.rstrip('.ipynb')
@@ -70,11 +132,21 @@ class ThisWorks(Magics):
 
         with open(profile_name + ".yaml", "w") as file:
             file.write('\n'.join(profile_contents))
-    
-        check_call(["hit","build",profile_name+".yaml"])
+
+        call_buffered(["hit", "build", profile_name + ".yaml"])
         ipython_path = os.path.join(profile_name, 'bin', 'ipython')
-        print "calling", ipython_path, "notebook", notebook_name
-        check_call([ipython_path, "notebook", notebook_name])
+        port = random_port()
+        ipython_cmd = [ipython_path, "notebook", os.getcwd(),
+            '--no-browser', '--port=%i' % port, '--ip=localhost',
+        ]
+        print("calling %s" % ' '.join(ipython_cmd))
+        env = os.environ.copy()
+        env['THISWORKS'] = '1'
+        ipython_process = Popen(ipython_cmd, stdout=PIPE, stderr=STDOUT, env=env)
+        wait_for_port('localhost', port)
+        url = "http://localhost:%i/notebooks/%s" % (port, notebook_name)
+        display(Javascript(REDIRECT_TEMPLATE.format(url=url)))
+        buffer_output(ipython_process)
 
 
 def load_ipython_extension(ipython):
